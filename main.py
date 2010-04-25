1250716eae6738ad12172c1a9bd473c8ba971f70
Evan Klitzke <evan@eklitzke.org>
2010-04-25 01:58:59 -0700

    more rooty things

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 import os
 import optparse
 import yaml
@@ -8,19 +9,48 @@ import tornado.web
 
 import toffee
 
+class listen_context(object):
+
+	def __init__(self, server, port, config):
+		self.server = server
+		self.port = port
+		self.config = config
+		self.setuid = None
+		if os.getuid() == 0:
+			if not config.get('uid'):
+				print >> sys.stderr, 'cowardly refusing to run as root'
+				sys.exit(1)
+			self.setuid = config['uid']
+
+	def __enter__(self):
+		self.server.listen(port)
+		if self.setuid:
+			os.setuid(self.setuid)
+
+	def __exit__(self, exc_type, exc_value, exc_traceback):
+		return
 
 if __name__ == '__main__':
-	defaultport = 80 if os.geteuid() == 0 else 8888
 	parser = optparse.OptionParser()
-	parser.add_option('-p', '--port', dest='port', default=defaultport, help='the port to listen on')
+	parser.add_option('-p', '--port', dest='port', default=0, type='int', help='the port to listen on')
 	parser.add_option('-c', '--config', dest='config', default='config.yaml', help='Configuration file to use')
 	opts, args = parser.parse_args()
 
 	settings = {}
 	if os.path.exists(opts.config):
-		settings.update(yaml.load(open(opts.config)))
-		application = tornado.web.Application(toffee.routes, **settings)
+		config = yaml.load(open(opts.config))
+	else:
+		print >> sys.stderr, 'no config file specified (or path did not exist), exiting'
+		sys.exit(1)
+
+	settings.update(config)
+	application = tornado.web.Application(toffee.routes, **settings)
+
+	if opts.port:
+		port = opts.port
+	else:
+		port = settings.get('port', 8080)
 
 	server = tornado.httpserver.HTTPServer(application)
-	server.listen(opts.port)
-	tornado.ioloop.IOLoop.instance().start()
+	with listen_context(server, opts.port, config):
+		tornado.ioloop.IOLoop.instance().start()
