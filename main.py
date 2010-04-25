b825f13e7e2e8a6fd7f23ad70c8072334d779c3a
Evan Klitzke <evan@eklitzke.org>
2010-04-25 02:07:00 -0700

    add multithreading

--- main.py
+++ main.py
@@ -9,30 +9,10 @@ import tornado.web
 
 import toffee
 
-class listen_context(object):
-
-	def __init__(self, server, port, config):
-		self.server = server
-		self.port = port
-		self.config = config
-		self.setuid = None
-		if os.getuid() == 0:
-			if not config.get('uid'):
-				print >> sys.stderr, 'cowardly refusing to run as root'
-				sys.exit(1)
-			self.setuid = config['uid']
-
-	def __enter__(self):
-		self.server.listen(port)
-		if self.setuid:
-			os.setuid(self.setuid)
-
-	def __exit__(self, exc_type, exc_value, exc_traceback):
-		return
-
 if __name__ == '__main__':
 	parser = optparse.OptionParser()
 	parser.add_option('-p', '--port', dest='port', default=0, type='int', help='the port to listen on')
+	parser.add_option('-t', '--threads', dest='threads', default=4, type='int', help='how many threads to spawn')
 	parser.add_option('-c', '--config', dest='config', default='config.yaml', help='Configuration file to use')
 	opts, args = parser.parse_args()
 
@@ -49,8 +29,9 @@ if __name__ == '__main__':
 	if opts.port:
 		port = opts.port
 	else:
-		port = settings.get('port', 8080)
-
-	server = tornado.httpserver.HTTPServer(application)
-	with listen_context(server, opts.port, config):
-		tornado.ioloop.IOLoop.instance().start()
+		port = settings.get('port', 8000)
+	
+	for x in xrange(opts.threads):
+		server = tornado.httpserver.HTTPServer(application)
+		server.listen(port + x)
+	tornado.ioloop.IOLoop.instance().start()
