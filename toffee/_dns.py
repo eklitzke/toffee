ff843f3e5a033f92ad2b84163d6ea1ca85d7c3e3
Evan Klitzke <evan@eklitzke.org>
2010-05-02 01:26:22 -0700

    fancy orm

--- toffee/_dns.py
+++ toffee/_dns.py
@@ -25,22 +25,35 @@ class ResourceRecord(object):
     def validate(self):
         return self.dns.validate()
 
+    @classmethod
+    def from_wire(cls, data):
+        return cls.dns_cls.from_wire(cls.rdclass, cls.rdtype, buffer(data), 0, len(data))
+
 class A(ResourceRecord):
 
     rdtype = 1
-
+    dns_cls = dns.rdtypes.IN.A.A
+ 
     def __init__(self, address):
-        self.dns = dns.rdtypes.IN.A.A(self.rdclass, self.rdtype, address)
+        self.dns = self.dns_cls(self.rdclass, self.rdtype, address)
         self.validate()
 
 class SOA(ResourceRecord):
 
     rdtype = 6
+    dns_cls = dns.rdtypes.ANY.SOA.SOA
 
     def __init__(self, mname, rname, serial, refresh, retry, expire, minimum):
         mname = self.to_dns_name(mname)
         rname = self.to_dns_name(rname)
-        self.dns = dns.rdtypes.ANY.SOA.SOA(self.rdclass, self.rdtype, mname, rname, serial, refresh, retry, expire, minimum)
+        self.dns = self.dns_cls(self.rdclass, self.rdtype, mname, rname, serial, refresh, retry, expire, minimum)
         self.validate()
 
-__all__ = ['A', 'SOA']
+    @classmethod
+    def increment_serial(cls, data):
+        """Increment the serial in an SOA record"""
+        data = cls.from_wire(data)
+        data.serial += 1
+        buf = StringIO.StringIO()
+        data.to_wire(buf)
+        return buffer(buf.getvalue()), data.serial
--- toffee/named/sql.py
+++ toffee/named/sql.py
@@ -1,71 +1,39 @@
 from contextlib import contextmanager
-import sqlite3
+import sqlalchemy
+
 import toffee._dns as dns
+from toffee.named.tables import *
 
-DEFAULT_TTL = 120
+bind_engine(sqlalchemy.create_engine("sqlite:///:memory:", echo=True))
+session = Session()
 
-SUPPORTED_TLDS = ['.com', '.net', '.org', '.example']
-
-_engine = sqlite3.connect('test.db', isolation_level='DEFERRED')
-_engine.row_factory = sqlite3.Row
-
-def maybe_one(query):
-    rows = list(query)
-    if len(rows) == 0:
-        return None
-    elif len(rows) == 1:
-        return rows[0]
-    else:
-        raise ValueError("Got %d rows (expected one or zero)" % (len(rows),))
-
-def just_one(query):
-    rows = list(query)
-    if len(rows) == 1:
-        return rows[0]
-    else:
-        raise ValueError("Got %d rows (expected exactly one)" % (len(rows),))
-
-@contextmanager
-def cursor(engine=None):
-    engine = engine or _engine
-    c = engine.cursor()
-    try:
-        yield c
-    finally:
-        c.close()
-
-@contextmanager
-def read_txn(self):
-    try:
-        yield self.conn.cursor()
-    finally:
-        self.conn.rollback()
-
-@contextmanager
-def write_txn(self):
-    try:
-        yield self.conn.cursor()
-    except:
-        self.conn.rollback()
-        raise
-    else:
-        self.conn.commit()
+def txn(func):
+    def inner(cls, name, *args, **kwargs):
+        try:
+            func(cls, name, *args, **kwargs)
+            SOA.bump_serial(cls.to_base_name(name))
+        except:
+            session.rollback()
+            raise
+        else:
+            session.commit()
+    inner.__name__ = func.__name__
+    inner.__doc__ = func.__doc__
+    return inner
 
 class DNSResource(object):
 
     qtype = None
+    default_ttl = 120
 
     @classmethod
-    def remove_dups(cls, name, rdata, qtype=None):
-        qtype = qtype or cls.qtype
-        with cursor() as c:
-            c.execute('DELETE FROM responses WHERE name = ? AND qclass = 1 AND qtype = ? AND rdata = ?', (name, qtype, rdata))
-  
-    @classmethod
-    def insert_record(cls, name, ttl, rdata, qtype=None):
-        qtype = qtype or cls.qtype
-        with cursor() as c:
-            c.execute('INSERT INTO responses (name, qclass, qtype, ttl, rdata) VALUES (?, 1, ?, ?, ?)', (name, qtype, ttl, rdata))
+    def filter_qtype(cls, qclass=1, name=None):
+        print Zone.qclass == qclass
+        q = session.query(Zone).filter_by(qclass=qclass).filter_by(qtype=cls.qtype)
+        if name is not None:
+            return q.filter(Zone.name == name)
+        else:
+            return q
 
     @classmethod
     def to_base_name(cls, name):
@@ -74,68 +42,55 @@ class DNSResource(object):
         return '.'.join(split_name[-3:]) # something like foo.com. or bar.org.
 
     @classmethod
-    def get_records_by_type_and_name(cls, name):
-        with cursor() as c:
-            return c.execute('SELECT COUNT(*) AS count FROM responses WHERE qclass = 1 AND qtype = ? AND name = ?', (cls.qtype, name))
-    
-    @classmethod
-    def update_ttl_and_rdata_by_id(cls, row_id, ttl, rdata):
-        with cursor() as c:
-            return c.execute('UPDATE responses SET ttl = ?, rdata = ? WHERE id = ?', (ttl, rdata, row_id))
-     
-    @classmethod
-    def insert_or_update(cls, name, ttl, rdata):
-        row = maybe_one(cls.get_records_by_type_and_name(name))
-        if row:
-            return cls.update_ttl_and_rdata_by_id(row['id'], ttl, rdata)
-        else:
-            return cls.insert_or_update(name, ttl, rdata)
+    def new_record(cls, name, rdata, ttl=None):
+        kwargs = {'name': name, 'qtype': cls.qtype, 'rdata': rdata}
+        if ttl is not None:
+            kwargs['ttl'] = ttl
+        record = Zone(**kwargs)
+        session.add(record)
+        return record
 
     @classmethod
-    def check_record_uniqueness(cls, name):
-        with cursor() as c:
-            row = just_one(c.execute('SELECT COUNT(*) AS count FROM responses WHERE qclass = 1 AND qtype = ? AND name = ?', (cls.qtype, name)))
-            if row['count'] > 0:
-                raise ValueError("Expected a unique record for name %r with qtype %d" % (name, cls.qtype))
-
-    @classmethod
-    def increment_serial(cls, name):
+    def bump_serial(cls, name):
         base_name = cls.to_base_name(name)
-        with cursor() as c:
-            row = just_one(c.execute('SELECT id FROM responses WHERE qclass = 1 AND qtype = 6 AND name = ?', (base_name,)))
-            c.execute('UPDATE responses SET ttl = ttl + 1 WHERE id = ?', (row['id'],))
-
-    @classmethod
-    def delete_one(cls, name):
-        records = list(cls.get_records_by_type_and_name(name))
-        if len(records) > 1:
-            raise ValueError("Can only use delete_one to delete a unique record")
-        if records:
-            with cursor() as c:
-                c.execute('DELETE FROM responses where id = ?', (records[0]['id'],))
-            return True
-        return False
+        soa = session.query.filter(Zone.qclass == 1).filter(Zone.qtype == SOA.qtype).filter(Zone.name == base_name).one()
+        soa.serial = soa.serial + 1
 
 class A(DNSResource):
 
     qtype = 1
 
     @classmethod
-    def update(cls, name, address, ttl=DEFAULT_TTL):
-        rdata = dns.A(address)
-        cls.insert_or_update(name, ttl, rdata)
-        cls.increment_serial(name)
+    @txn
+    def update(cls, name, address):
+        record = cls.filter_qtype(name=name).first()
+        rdata = dns.A(address).as_buffer()
+        if record:
+            record.rdata = rdata
+        else:
+            record = cls.new_record(name, rdata)
+        return record.id
 
 class SOA(DNSResource):
 
     qtype = 6
 
     @classmethod
-    def create(cls, name, mname, rname, refresh, retry, expire, minimum, ttl=DEFAULT_TTL, serial=1):
+    @txn
+    def update(cls, name, mname, rname, serial=0, refresh=86400, retry=3600, expire=3600000, minimum=120):
         if name != cls.to_base_name(name):
             raise ValueError("Cannot create an SOA record for a non-TLD")
-        rdata = dns.SOA(mname, rname, serial, refresh, retry, expire, minimum).as_buffer()
-        cls.check_record_uniqueness(name)
-        cls.insert_record(name, ttl, rdata)
 
-__all__ = ['just_one', 'maybe_one', 'read_txn', 'write_txn', 'A', 'SOA']
+        existing = cls.filter_qtype(name=name).first()
+        if existing:
+            session.delete(existing)
+
+        rdata = dns.SOA(mname, rname, serial, refresh, retry, expire, minimum).as_buffer()
+        cls.new_record(name, rdata, ttl=0)
+
+    @classmethod
+    def bump_serial(cls, name):
+        record = cls.filter_qtype(name=name).one()
+        rdata, serial = dns.SOA.increment_serial(record.rdata)
+        record.rdata = rdata
+        return serial
